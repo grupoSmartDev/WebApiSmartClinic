741027703637bd6f39c38f0c1cf526ead16d7443
SmartCrm <gruposmartdesenvolvimentos@gmail.com>
2024-09-03 22:08:46 -0300

    Initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiSmartClinic.Models;
+
+namespace WebApiSmartClinic.Data;
+
+public class AppDbContext : DbContext
+{
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+  {
+
+  }
+
+  public DbSet<AutorModel> Autores { get; set; }
+  public DbSet<LivroModel> Livros { get; set; }
+  public DbSet<StatusModel> Status { get; set; }  
+}
---
+++ Migrations/20240904010403_addStatuss.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiSmartClinic.Migrations
+{
+    /// <inheritdoc />
+    public partial class addStatuss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Status",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Legenda = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cor = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Status", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Status");
+        }
+    }
+}

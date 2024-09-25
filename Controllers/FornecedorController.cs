fda75780f12c65b7cb4599ed65f17c39c492e3fe
SmartCrm <gruposmartdesenvolvimentos@gmail.com>
2024-09-24 21:32:43 -0300

    ajuste simples

--- Controllers/FornecedorController.cs
+++ Controllers/FornecedorController.cs
@@ -16,35 +16,35 @@ namespace WebApiSmartClinic.Controllers
             _fornecedor = fornecedor;
         }
 
-        [HttpGet("ListarFornecedor")]
+        [HttpGet("Listar")]
         public async Task<ActionResult<ResponseModel<List<FornecedorModel>>>> ListarFornecedor()
         {
             var fornecedor = await _fornecedor.ListarFornecedor();
             return Ok(fornecedor);
         }
 
-        [HttpGet("BuscarFornecedorPorId/{idFornecedor}")]
+        [HttpGet("BuscarPorId/{idFornecedor}")]
         public async Task<ActionResult<ResponseModel<List<FornecedorModel>>>> BuscarFornecedorPorId(int idFornecedor)
         {
             var fornecedor = await _fornecedor.BuscarFornecedorPorId(idFornecedor);
             return Ok(fornecedor);
         }
 
-        [HttpPost("CriarFornecedor")]
+        [HttpPost("Criar")]
         public async Task<ActionResult<ResponseModel<List<FornecedorModel>>>> CriarFornecedor(FornecedorCreateDto fornecedorCreateDto)
         {
             var fornecedor = await _fornecedor.CriarFornecedor(fornecedorCreateDto);
             return Ok(fornecedor);
         }
 
-        [HttpPut("EditarFornecedor")]
+        [HttpPut("Editar")]
         public async Task<ActionResult<ResponseModel<List<FornecedorModel>>>> EditarFornecedor(FornecedorEdicaoDto fornecedorEdicaoDto)
         {
             var fornecedor = await _fornecedor.EditarFornecedor(fornecedorEdicaoDto);
             return Ok(fornecedor);
         }
 
-        [HttpDelete("DeleteFornecedor/{idFornecedor}")]
+        [HttpDelete("Delete/{idFornecedor}")]
         public async Task<ActionResult<ResponseModel<List<FornecedorModel>>>> DeleteFornecedor(int idFornecedor)
         {
             var fornecedor = await _fornecedor.DeleteFornecedor(idFornecedor);
--- Models/FornecedorModel.cs
+++ Models/FornecedorModel.cs
@@ -1,36 +1,35 @@
 
-namespace WebApiSmartClinic.Models
-{
-    public class FornecedorModel
-    {
-        public int Id { get; set; }
-        public string Razao { get; set; }
-        public string Fantasia { get; set; }
-        public string Tipo { get; set; }
-        public string EstadoCivil { get; set; }
-        public string Sexo { get; set; }
-        public string IE { get; set; }
-        public string IM { get; set; }
-        public string CPF { get; set; }
-        public string CNPJ { get; set; }
-        public string Pais { get; set; }
-        public string UF { get; set; }
-        public string Cidade { get; set; }
-        public string Bairro { get; set; }
-        public string Complemento { get; set; }
-        public string Logradouro { get; set; }
-        public string NrLogradouro { get; set; }
-        public string CEP { get; set; }
-        public string Celular { get; set; }
-        public string TelefoneFixo { get; set; }
-        public string Banco { get; set; }
-        public string Agencia { get; set; }
-        public string Conta { get; set; }
-        public string TipoPIX { get; set; }
-        public string ChavePIX { get; set; }
-        public string Email { get; set; }
-        public DateTime? DataNascimento { get; set; }
+namespace WebApiSmartClinic.Models;
 
-        //public ICollection<Financ_Pagar> Financ_Pagar { get; set; }
-    }
+public class FornecedorModel
+{
+    public int Id { get; set; }
+    public string Razao { get; set; }
+    public string Fantasia { get; set; }
+    public string Tipo { get; set; }
+    public string EstadoCivil { get; set; }
+    public string Sexo { get; set; }
+    public string IE { get; set; }
+    public string IM { get; set; }
+    public string CPF { get; set; }
+    public string CNPJ { get; set; }
+    public string Pais { get; set; }
+    public string UF { get; set; }
+    public string Cidade { get; set; }
+    public string Bairro { get; set; }
+    public string Complemento { get; set; }
+    public string Logradouro { get; set; }
+    public string NrLogradouro { get; set; }
+    public string CEP { get; set; }
+    public string Celular { get; set; }
+    public string TelefoneFixo { get; set; }
+    public string Banco { get; set; }
+    public string Agencia { get; set; }
+    public string Conta { get; set; }
+    public string TipoPIX { get; set; }
+    public string ChavePIX { get; set; }
+    public string Email { get; set; }
+    public DateTime? DataNascimento { get; set; }
+
+    //public ICollection<Financ_Pagar> Financ_Pagar { get; set; }
 }

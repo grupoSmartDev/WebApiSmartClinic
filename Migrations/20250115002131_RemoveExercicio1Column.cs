7a11f83737f0cc2132ea9bae3bc9a8a1814e764f
SmartCrm <gruposmartdesenvolvimentos@gmail.com>
2025-01-14 21:29:15 -0300

    remove table

---
+++ Migrations/20250115002131_RemoveExercicio1Column.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiSmartClinic.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveExercicio1Column : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Primeiro remove a chave estrangeira
+            migrationBuilder.DropForeignKey(
+                name: "FK_Exercicio_Evolucoes_EvolucaoId1",
+                table: "Exercicio");
+
+            // Remove o índice
+            migrationBuilder.DropIndex(
+                name: "IX_Exercicio_EvolucaoId1",
+                table: "Exercicio");
+
+            // Agora sim remove a coluna
+            migrationBuilder.DropColumn(
+                name: "EvolucaoId1",
+                table: "Exercicio");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EvolucaoId1",
+                table: "Exercicio",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ Migrations/20250115002436_RemoveExercicio1Columnsd.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiSmartClinic.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveExercicio1Columnsd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Primeiro remove a chave estrangeira
+            migrationBuilder.DropForeignKey(
+                name: "FK_Exercicio_Evolucoes_EvolucaoId1",
+                table: "Exercicio");
+
+            // Remove o índice
+            migrationBuilder.DropIndex(
+                name: "IX_Exercicio_EvolucaoId1",
+                table: "Exercicio");
+
+            // Agora sim remove a coluna
+            migrationBuilder.DropColumn(
+                name: "EvolucaoId1",
+                table: "Exercicio");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

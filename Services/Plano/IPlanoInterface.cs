356e3f2df531f5b286435d893b353f1959e74456
SmartCrm <gruposmartdesenvolvimentos@gmail.com>
2025-02-14 08:31:40 -0300

    att
    bug

--- Services/Plano/IPlanoInterface.cs
+++ Services/Plano/IPlanoInterface.cs
@@ -12,6 +12,6 @@ namespace WebApiSmartClinic.Services.Plano
         Task<ResponseModel<List<PlanoModel>>> Criar(PlanoCreateDto planoCreateDto, int pageNumber = 1, int pageSize = 10);
         Task<ResponseModel<PlanoModel>> PlanoParaPaciente(PlanoCreateDto planoCreateDto);
         Task<ResponseModel<List<PlanoModel>>> Editar(PlanoEdicaoDto planoEdicaoDto, int pageNumber = 1, int pageSize = 10);
-        Task<ResponseModel<List<PlanoModel>>> Listar(int pageNumber = 1, int pageSize = 10, int? codigoFiltro = null, string? descricaoFiltro = null, bool paginar = true);
+        Task<ResponseModel<List<PlanoModel>>> Listar(int pageNumber = 1, int pageSize = 10, int? codigoFiltro = null, string? descricaoFiltro = null, bool paginar = true, bool paraPaciente = false);
     }
 }

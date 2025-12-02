fb774bb5ff21275bc617aa4b7097b605d4627e2f
BrunoBarrichello <99931716+BrunoBarrichello@users.noreply.github.com>
2025-12-02 18:58:58 -0300

    att modulo de usuarios

--- Services/Auth/IAuthInterface.cs
+++ Services/Auth/IAuthInterface.cs
@@ -9,7 +9,7 @@ namespace WebApiSmartClinic.Services.Auth
         Task<object> RegisterAsync(UserCreateRequest model, string? userKey);
         Task<object> GetAllUsersAsync(int page, int pageSize, string? filter);
         Task<object> GetUserByIdAsync(string id, ClaimsPrincipal currentUser);
-        Task<object> UpdateUserAsync(string id, UserUpdateRequest model, IFormFile? profilePicture, ClaimsPrincipal currentUser);
+        Task<object> UpdateUserAsync(string id, UserUpdateRequest model, ClaimsPrincipal currentUser);
         Task<object> Editar(string id, UserUpdateRequest model);
         Task<object> DeleteUserAsync(string id);
     }
